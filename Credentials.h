fa7ef5b452863d4bc460909ea3f5c940ad5e7aa4
RIN67630 <lazlo.lebrun@googlemail.com>
2023-11-30 22:48:41 +0100

    Create Credentials.h

---
+++ Credentials.h
@@ -0,0 +1,7 @@
+#define WIFI_SSID            "Secret"
+#define WIFI_PASS            "Secret"
+
+// ***Time zones***
+#define NTP_SERVER "de.pool.ntp.org"
+#define MYTZ TZ_Europe_Paris
+#define TZ   1                // (utc+) TZ in hours
